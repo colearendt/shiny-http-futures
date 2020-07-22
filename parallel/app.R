a3f4a494c0792eceddf6a72d73ceed1fd0f4ffc6
Cole Arendt <cole.arendt@outlook.com>
2020-07-22 17:19:18 -0400

    implement a parallel strategy

--- parallel/app.R
+++ parallel/app.R
@@ -3,8 +3,8 @@ library(future)
 library(promises)
 plan(multiprocess)
 
-helper <- function(iteration = 0) {
-    res <- httr::GET("https://colorado.rstudio.com/rsc/plumber-load-test/echo?sleep=3&msg='hello'&copies=10")
+helper <- function(iteration = 0, duration = 2) {
+    res <- httr::GET(glue::glue("https://colorado.rstudio.com/rsc/plumber-load-test/echo?sleep={duration}&msg='hello'&copies=10"))
     return(res)
 }
 
@@ -14,10 +14,12 @@ ui <- fluidPage(
 
     sidebarLayout(
         sidebarPanel(
-            actionButton("try", "Try Again")
+            actionButton("try", "Try Again"),
+            actionButton("done", "Click when Done")
         ),
 
         mainPanel(
+            h3(textOutput("time_text")),
             textOutput("output1"),
             textOutput("output2"),
             textOutput("output3"),
@@ -30,7 +32,25 @@ ui <- fluidPage(
 server <- function(input, output) {
     time_taken <- reactiveVal(0)
 
-    button_click <- reactiveVal(0)
+    start_time <- reactiveVal(0)
+    done_time <- reactiveVal(0)
+
+    observeEvent(input$try, {
+        start_time(Sys.time())
+        done_time(0)
+    })
+
+    observeEvent(input$done, {
+        done_time(Sys.time())
+    })
+
+    output$time_text <- renderText({
+        if(done_time() == 0) {
+            "Time not computed yet..."
+        } else {
+            glue::glue("Requests took {done_time() - start_time()} seconds")
+        }
+    })
 
     var1 <- eventReactive(input$try, future({helper(1)}))
     var2 <- eventReactive(input$try, future({helper(2)}))
@@ -43,7 +63,6 @@ server <- function(input, output) {
     output$output3 <- renderText(var3() %...>% print() %...>% capture.output())
     output$output4 <- renderText(var4() %...>% print() %...>% capture.output())
     output$output5 <- renderText(var5() %...>% print() %...>% capture.output())
-
 }
 
 shinyApp(ui = ui, server = server)
